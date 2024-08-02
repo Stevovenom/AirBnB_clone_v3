683f6d478fb873ede88bf0b5b0de8cfc2d8e8a05
stevovenom <stephenokoth2002@gmail.com>
2024-08-02 06:57:41 -0700

    error fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
 # api/v1/views/__init__.py
 
+from api.v1.views import index
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views import index

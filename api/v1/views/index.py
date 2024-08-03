12c1dfd2be6f60f64a403813e78985d59998d3c2
bobuzy <bobuzytech@gmail.com>
2024-08-03 23:39:28 +0000

    Update index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,13 +21,20 @@ def status():
 @app_views.route("/stats", strict_slashes=False)
 def stats():
     """Retrieve the number of each object by type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
     stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "state": storage.count("State"),
-        "users": storage.count("User")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "state": storage.count(State),
+        "users": storage.count(User)
     }
 
     response = jsonify(stats)

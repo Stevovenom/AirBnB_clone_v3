ce8a1a4398d7dce8dd90b47d5aafa9417efe1d9f
bobuzy <bobuzytech@gmail.com>
2024-08-03 22:52:58 +0000

    Allow cross-origin resource sharing

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,9 @@ from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views, url_prefix='/api/v1')
 app.url_map.strict_slashes = False
 

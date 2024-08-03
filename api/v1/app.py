2ec8d3ef2d8a573d1736ebd6ecdea401ee13fc97
bobuzy <bobuzytech@gmail.com>
2024-08-03 22:59:06 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,8 +9,9 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.url_map.strict_slashes = False
 
 

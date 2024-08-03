70698fddecbc28191560328b23b98c6ae2a145d0
bobuzy <bobuzytech@gmail.com>
2024-08-03 13:05:41 +0000

    Update init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
-# api/v1/views/__init__.py
-
-from api.v1.views import index
+#!/usr/bin/python3
+"""Create app_views Blueprint"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__)
+
+from api.v1.views.index import *

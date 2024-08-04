9a7e03d241d839e1798fcfffc339a47f437cfcfd
stevovenom <stephenokoth2002@gmail.com>
2024-08-04 05:44:24 -0700

    trial fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""State objects that handles all default RESTFul API actions"""
+"""
+State objects that handle all default RESTFul API actions.
+"""
 
 from flask import jsonify, request, abort
 from models import storage
@@ -9,14 +11,24 @@ from api.v1.views import app_views
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
-    """Retrieves the list of all State objects"""
+    """
+    Retrieves the list of all State objects.
+    """
     states = [state.to_dict() for state in storage.all(State).values()]
     return jsonify(states)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
-    """Retrieves a State object"""
+    """
+    Retrieves a State object.
+
+    Args:
+        state_id (str): The ID of the State to retrieve.
+
+    Returns:
+        json: The state object in JSON format or a 404 error if not found.
+    """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
@@ -26,7 +38,16 @@ def get_state(state_id):
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
-    """Deletes a State object"""
+    """
+    Deletes a State object.
+
+    Args:
+        state_id (str): The ID of the State to delete.
+
+    Returns:
+        json: An empty dictionary with a status code 200 or a 404 error if
+        not found.
+    """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
@@ -37,7 +58,13 @@ def delete_state(state_id):
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
-    """Creates a State"""
+    """
+    Creates a State.
+
+    Returns:
+        json: The newly created state object in JSON format or appropriate
+        errors.
+    """
     if not request.json:
         abort(400, description="Not a JSON")
     if 'name' not in request.json:
@@ -50,7 +77,15 @@ def create_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
-    """Updates a State object"""
+    """
+    Updates a State object.
+
+    Args:
+        state_id (str): The ID of the State to update.
+
+    Returns:
+        json: The updated state object in JSON format or appropriate errors.
+    """
     state = storage.get(State, state_id)
     if not state:
         abort(404)

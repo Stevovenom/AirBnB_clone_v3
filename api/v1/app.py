02c434a9ac2fef042a5d75a2c971a857ffe9853b
Benedict Abudu <115162603+bobuzy@users.noreply.github.com>
2024-08-04 23:46:38 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-"""
-Module defines a Flask application
-"""
+"""Flask server (variable app)"""
 
 from flask import Flask, jsonify
 from models import storage
+from os import getenv
 from api.v1.views import app_views
-import os
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 
 @app.errorhandler(404)
-def not_found_error(error):
-    """ Handles error 404 """
-    return jsonify({"error": "Not found"}), 404
+def page_not_found(error):
+    """Return a JSON response for a 404 error."""
+    return jsonify({'error': 'Not found'}), 404
 
 
 @app.teardown_appcontext
-def teardown(exception=None):
-    """ Closes the storage """
+def teardown_db(exception=None):
+    """Close the SQLAlchemy session."""
     storage.close()
 
 

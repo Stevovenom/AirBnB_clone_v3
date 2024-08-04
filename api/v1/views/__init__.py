43000affcf2a3030098cbeff980db0e0dbca3ce1
stevovenom <stephenokoth2002@gmail.com>
2024-08-04 07:43:43 -0700

    restoration

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Create app_views Blueprint"""
 
-from api.v1.views.states import *
-from api.v1.views.index import *
+
 from flask import Blueprint
-app_views = Blueprint('app_views', __name__)
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+from api.v1.views.index import *

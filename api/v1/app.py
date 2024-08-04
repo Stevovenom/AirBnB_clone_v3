4e3c69bc294cba9f37d5943b86878314d5657e21
bobuzy <bobuzytech@gmail.com>
2024-08-04 23:02:14 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,19 +15,20 @@ CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close the SQLAlchemy session."""
+    storage.close()
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     """Return a JSON response for a 404 error."""
     return jsonify({'error': 'Not found'}), 404
 
 
-@app.teardown_appcontext
-def teardown_db(exception=None):
-    """Close the SQLAlchemy session."""
-    storage.close()
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True, debug=True)
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=int(port), threaded=True)

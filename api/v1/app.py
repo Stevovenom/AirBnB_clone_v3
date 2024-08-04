d6630fc620e880500df5a6a32782d34acc7a7aa8
Benedict Abudu <115162603+bobuzy@users.noreply.github.com>
2024-08-04 23:45:20 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-"""Flask server (variable app)"""
+"""
+Module defines a Flask application
+"""
 
 from flask import Flask, jsonify
 from models import storage
-from os import getenv
 from api.v1.views import app_views
-from flask_cors import CORS
+import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """Return a JSON response for a 404 error."""
-    return jsonify({'error': 'Not found'}), 404
+def not_found_error(error):
+    """ Handles error 404 """
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
-def teardown_db(exception=None):
-    """Close the SQLAlchemy session."""
+def teardown(exception=None):
+    """ Closes the storage """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+if __name__ == '__main__':
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True, debug=True)

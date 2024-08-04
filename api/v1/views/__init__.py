d24d3d46c12216c9b28bec12412005b3c25dd928
stevovenom <stephenokoth2002@gmail.com>
2024-08-04 06:10:00 -0700

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,8 @@
 """
 Init file for views module
 """
-
+from api.v1.views.index import *
 from api.v1.views.states import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Import the views after creating the blueprint

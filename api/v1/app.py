a41c897404bacd6eba18b8cd09d8884e6b1de6e1
bobuzy <bobuzytech@gmail.com>
2024-08-05 18:35:43 +0000

    Reverse changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,11 +9,10 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-
+app.register_blueprint(app_views, url_prefix='/api/v1')
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -21,14 +20,16 @@ def teardown_db(exception):
     """Close the SQLAlchemy session."""
     storage.close()
 
-
 @app.errorhandler(404)
 def page_not_found(error):
     """Return a JSON response for a 404 error."""
     return jsonify({'error': 'Not found'}), 404
 
-
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=int(port), threaded=True)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
